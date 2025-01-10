056094763bf5854bf99ac689829442eef4522b7d
João_A.11 <jarlpereira29@gmail.com>
2025-01-10 10:58:16 -0300

    Adiciona arquivos necessarios

---
+++ app/src/main/java/com/joao_ar11/interfacenetflix/MainActivity.java
@@ -0,0 +1,14 @@
+package com.joao_ar11.interfacenetflix;
+
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+public class MainActivity extends ComponentActivity {
+
+    @Override
+    protected void onCreate( Bundle savedInstances ) {
+        super.onCreate(savedInstances);
+        setContentView(R.layout.activity_main);
+    }
+}
